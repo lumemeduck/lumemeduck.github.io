e559cee0e0fbacd1d461c976c4c13b328e291dc6
LMD Network <118551627+lumemeduck@users.noreply.github.com>
2025-10-21 14:32:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,12 @@ window.addEventListener('resize', () => {
   if (window.innerWidth > 768) closeMenu();
 });
 
+window.addEventListener('DOMContentLoaded', (event) => {
+  if (window.innerWidth > 768) {
+    menu.style.display = 'flex';
+  }
+});
+
 //  REMOVE LOADER
 window.addEventListener('load', () => document.body.classList.add('loaded'));
 
